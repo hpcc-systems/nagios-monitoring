2547fea044a71d8e738c524a97e86c744efdc0de
Richard Chapman <rchapman@hpccsystems.com>
2014-05-05 15:43:00 +0100

    Split off 5.0.0
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -1,9 +1,9 @@
 ###
 ## Version Information
 ###
-set ( HPCC_NM_MAJOR 4 )
-set ( HPCC_NM_MINOR 3 )
+set ( HPCC_NM_MAJOR 5 )
+set ( HPCC_NM_MINOR 1 )
 set ( HPCC_NM_POINT 0 )
 set ( HPCC_NM_MATURITY "trunk" )
-set ( HPCC_NM_SEQUENCE 1 )
+set ( HPCC_NM_SEQUENCE 0 )
 ###
