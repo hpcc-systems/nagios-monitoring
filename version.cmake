e758a8d738fc04462af6da958295b17513074331
Richard Chapman <rchapman@hpccsystems.com>
2014-07-07 17:16:32 +0100

    Nagios Monitoring 5.0.0-1 Gold
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -4,6 +4,6 @@
 set ( HPCC_NM_MAJOR 5 )
 set ( HPCC_NM_MINOR 0 )
 set ( HPCC_NM_POINT 0 )
-set ( HPCC_NM_MATURITY "rc" )
-set ( HPCC_NM_SEQUENCE 8 )
+set ( HPCC_NM_MATURITY "release" )
+set ( HPCC_NM_SEQUENCE 1 )
 ###
