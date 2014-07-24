f4bf861080a4731ea70d320b0bbcc7ec89026771
Gavin Halliday <gavin.halliday@lexisnexis.com>
2014-07-24 15:52:47 +0100

    Nagios Monitoring 4.2.8-rc4 Release Candidate 4
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -1,9 +1,9 @@
 ###
 ## Version Information
 ###
-set ( HPCC_NM_MAJOR 5 )
-set ( HPCC_NM_MINOR 1 )
-set ( HPCC_NM_POINT 0 )
-set ( HPCC_NM_MATURITY "trunk" )
-set ( HPCC_NM_SEQUENCE 0 )
+set ( HPCC_NM_MAJOR 4 )
+set ( HPCC_NM_MINOR 2 )
+set ( HPCC_NM_POINT 8 )
+set ( HPCC_NM_MATURITY "rc" )
+set ( HPCC_NM_SEQUENCE 4 )
 ###
