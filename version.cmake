5b75f8f16c5adde20d3ac05391592b9d2453b545
Richard Chapman <rchapman@hpccsystems.com>
2021-03-18 16:01:28 +0000

    Split off 8.0.x
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -3,9 +3,9 @@
 ###
 set ( HPCC_NAME "Nagios Monitoring" )
 set ( HPCC_PROJECT "nagios-monitoring" )
-set ( HPCC_MAJOR 7 )
-set ( HPCC_MINOR 13 )
+set ( HPCC_MAJOR 8 )
+set ( HPCC_MINOR 0 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "trunk" )
+set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 0 )
 ###
