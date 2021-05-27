bb35adda2e844ac3db0f85c0e5b58f4cfa5daafc
Richard Chapman <rchapman@hpccsystems.com>
2021-05-27 16:53:33 +0100

    Nagios Monitoring 8.2.0-rc1 Release Candidate 1
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -4,8 +4,8 @@
 set ( HPCC_NAME "Nagios Monitoring" )
 set ( HPCC_PROJECT "nagios-monitoring" )
 set ( HPCC_MAJOR 8 )
-set ( HPCC_MINOR 1 )
+set ( HPCC_MINOR 2 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "trunk" )
-set ( HPCC_SEQUENCE 0 )
+set ( HPCC_MATURITY "rc" )
+set ( HPCC_SEQUENCE 1 )
 ###
